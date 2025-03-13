7f1ba400a34e06eb82dd054cb91a8638fd769acc
Hitode <hitode@192.168.1.20.non-exists.ptr.local>
2025-03-13 10:22:01 +0700

    update code fix link our services

---
+++ src/wp-content/themes/accalia-child/includes/shin_custom_theme.php
@@ -0,0 +1,16 @@
+<?php
+add_action( 'template_redirect', 'custom_redirect_urls' );
+function custom_redirect_urls() {
+    $redirect_urls = array(
+        '/services' => '/our-services',
+    );
+
+    $request_uri = $_SERVER['REQUEST_URI'];
+
+    foreach ( $redirect_urls as $from_url => $to_url ) {
+        if ( $request_uri === $from_url ) {
+            wp_redirect( $to_url, 301 );
+            exit;
+        }
+    }
+}
