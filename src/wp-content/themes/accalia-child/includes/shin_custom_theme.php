c5a273f24e42f4f46076d45c1237a607a438b08e
admin@luna.com <admin@luna.com>
2025-03-13 11:01:43 +0700

    fixed issue

--- src/wp-content/themes/accalia-child/includes/shin_custom_theme.php
+++ src/wp-content/themes/accalia-child/includes/shin_custom_theme.php
@@ -4,15 +4,15 @@ function custom_redirect_urls() {
     if ( is_admin()) {
         return;
     }
-
     $redirect_urls = array(
-        '/services' => '/our-services',
+        '/services/' => '/our-services',
     );
 
     $request_uri = $_SERVER['REQUEST_URI'];
 
     foreach ( $redirect_urls as $from_url => $to_url ) {
-        if ( $request_uri === $from_url ) {
+        if ( $request_uri == $from_url ) {
+
             wp_redirect( $to_url, 301 );
             exit;
         }
